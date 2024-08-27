4c424c33d4ebadc7d71077a5ca20618a61363140
Cumbie <bcumbie@una.edu>
2024-08-27 13:53:36 -0500

    class on tuesday 27

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
-console.log('im on a node server, yo');
+console.log('im on a node server change that and that tanad f, yo');
 
 app.get('/', function (req, res) {
-  res.send('Hello Node from Ex on local dev box')
+  // res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html');
 })
 
 app.listen(5000)
