e92bc50156fa5e9cebf5f5f53ec8e6365487972d
Cumbie <bcumbie@una.edu>
2024-08-29 13:43:03 -0500

    got ejs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+app.set('view engine', 'ejs')
 app.use(express.static('./public/'))
 
 console.log('im on a node server change that and that tanad f, yo');
@@ -8,6 +8,14 @@ console.log('im on a node server change that and that tanad f, yo');
 app.get('/', function (req, res) {
   // res.send('Hello Node from Ex on local dev box')
   res.sendFile('index.html');
+})
+
+app.get('/ejs', (req,res)=>{
+``
+  res.render('index', {
+    myServerVariable : "something from server"
+  });
+
 })
 
 app.listen(5000)
