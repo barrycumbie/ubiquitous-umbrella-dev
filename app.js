707f64de214d5b38789ae8c3a8f40d59df05b24d
Cumbie <bcumbie@una.edu>
2024-09-03 13:46:27 -0500

    mongo cxn dead env

--- app.js
+++ app.js
@@ -2,8 +2,37 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('./public/'))
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.ENV.uri; 
 
-console.log('im on a node server change that and that tanad f, yo');
+console.log(uri);
+
+// console.log('im on a node server change that and that tanad f, yo');
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', function (req, res) {
   // res.send('Hello Node from Ex on local dev box')
@@ -16,6 +45,7 @@ app.get('/ejs', (req,res)=>{
     myServerVariable : "something from server"
   });
 
+  //can you get content from client...to console? 
 })
 
 app.listen(5000)
